bb408c2648856eb9c61cdb63e17592d694d2a7c2
Diego Ureña <diegojuc@gmail.com>
2021-10-22 05:15:28 -0600

    Se cambio biblioteca por una de PlatformIO

--- WiFICar/WiFiCar-ESP8266/src/definitions.h
+++ WiFICar/WiFiCar-ESP8266/src/definitions.h
@@ -1,5 +1,10 @@
 #ifndef DEFINITIONS_H
   #define DEFINITIONS_H
+
+  #define NODEMCU
+  //#define HUZZAH
+
+  // #define DEBUG
   
   #define TURBO_SPEED 200
   #define NORMAL_SPEED 150
@@ -7,13 +12,24 @@
   #define MAX_CONNECTION_TRIES 200
   #define USE_SERIAL Serial
   
-  #define GPIO_LEFT_1 4
-  #define GPIO_LEFT_2 15
-  #define GPIO_LEFT_SPEED 5
+  #ifdef HUZZAH
+    #define GPIO_LEFT_1 4
+    #define GPIO_LEFT_2 15
+    #define GPIO_LEFT_SPEED 5
+    
+    #define GPIO_RIGHT_1 12
+    #define GPIO_RIGHT_2 13
+    #define GPIO_RIGHT_SPEED 16
+  #endif
   
-  #define GPIO_RIGHT_1 12
-  #define GPIO_RIGHT_2 13
-  #define GPIO_RIGHT_SPEED 16
-  // #define DEBUG
+   #ifdef NODEMCU
+    #define GPIO_LEFT_1 16
+    #define GPIO_LEFT_2 3
+    #define GPIO_LEFT_SPEED 15
+    
+    #define GPIO_RIGHT_1 10
+    #define GPIO_RIGHT_2 6
+    #define GPIO_RIGHT_SPEED 4
+  #endif
 
 #endif
