d22cc69dd158fe4d6cdb28efac7668dc721d0713
Diego Ureña <diegojuc@gmail.com>
2021-10-22 04:54:12 -0600

    Se quitaron comentarios

--- WiFICar/WiFiCar-NodeMCU-ESP8266/src/main.cpp
+++ WiFICar/WiFiCar-NodeMCU-ESP8266/src/main.cpp
@@ -7,9 +7,6 @@
 #include "carControl.h"
 
 void setup() {
-	//pinMode(2, OUTPUT);     //Initialize GPIO2 pin as an output
-	//digitalWrite(2, HIGH);  // Turn the LED off by making the voltage HIGH 
-
 	carControlInit();
 	
 #ifdef DEBUG
@@ -35,9 +32,5 @@ void setup() {
 }
 
 void loop() {
-  //  digitalWrite(2, LOW);   // Turn the LED on by making the voltage LOW   
-  //  delay(1000);            // Wait for a second
-  //  digitalWrite(2, HIGH);  // Turn the LED off by making the voltage HIGH 
-  //  delay(2000);            // Wait for two seconds
 	webSocket.loop();
 }
